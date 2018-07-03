75873bdb0c84c3cd76473aa6b28886063a5dcbd9
Allenboy <32590722+haidragon@users.noreply.github.com>
2018-07-03 13:24:10 +0800

    Update 说明.c

--- 说明.c
+++ 说明.c
@@ -2,7 +2,7 @@ azazel反编译器
 ftrace函数追踪
 elfdemon 代码注入
 lpv,skeksi,saruman 病毒
-quenya 重建进程
+quenya 重建进程 有重定位注入
 text-infection 逆向text感染方法
 pax elf加固工具 https://pax.grsecurity.net/
 http://phrack.org/issues/57/9.html
